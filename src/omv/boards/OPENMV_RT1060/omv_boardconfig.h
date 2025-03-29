b8a47b72cf7e623fdcf64908fdd8ea10d2770bfc
dfordequan <dekchuen@foxmail.com>
2025-03-29 15:08:02 +0100

    disable histogram

--- src/omv/boards/OPENMV_RT1060/omv_boardconfig.h
+++ src/omv/boards/OPENMV_RT1060/omv_boardconfig.h
@@ -44,7 +44,7 @@
 #define OMV_BOSON_ENABLE                (1)
 #define OMV_LEPTON_ENABLE               (1)
 #define OMV_GENX320_ENABLE              (1)
-#define OMV_GENX320_EHC_ENABLE          (1)
+#define OMV_GENX320_EHC_ENABLE          (0)
 #define OMV_PAG7920_ENABLE              (1)
 #define OMV_PAJ6100_ENABLE              (1)
 #define OMV_FROGEYE2020_ENABLE          (1)
